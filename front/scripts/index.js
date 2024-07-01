0c82d46067db62e15002eea978452c9111e74660
agustinMontoya2 <agus7_montoya@hotmail.com>
2024-07-01 12:29:06 -0300

    primer push

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,77 @@
-console.log(tempData);
+import { tempData } from "./tempData.mjs";
+
+function crearObjeto(pelicula) {
+    const {title, year, director, duration, genre, rate, poster} = pelicula
+    
+    const imagen = document.createElement("img")
+    imagen.src = poster
+
+    const titulo = document.createElement("h4")
+    titulo.innerHTML = title
+    titulo.className= "tituloPelicula"
+
+    const puntuacion = document.createElement("p")
+    puntuacion.innerHTML = rate + "⭐"
+
+    const duracion = document.createElement("p")
+    duracion.innerHTML = duration
+
+    const divDescripcion = document.createElement("div")
+
+    const sinapsis = `${title} es una pelicula creada en el año ${year} del genero ${genre}`
+    const descripcion = document.createElement("p")
+    descripcion.innerHTML = sinapsis
+
+    const creador = document.createElement("h4")
+    creador.innerHTML = director
+
+    const tarjeta = document.createElement("div")
+    tarjeta.appendChild(titulo)
+    tarjeta.appendChild(imagen)
+    tarjeta.appendChild(creador)
+
+    const puntuacionDuracion = document.createElement("div")
+    puntuacionDuracion.appendChild(puntuacion)
+    puntuacionDuracion.appendChild(duracion)
+    puntuacionDuracion.className = "puntuacionDuracion"
+
+    divDescripcion.appendChild(puntuacionDuracion)
+
+    divDescripcion.appendChild(descripcion)
+    divDescripcion.className = "movieDate"
+    tarjeta.appendChild(divDescripcion)
+    tarjeta.className = "movieCard"
+
+    return tarjeta
+}
+
+function renderPeliculas() {
+    const recomendados = document.querySelector("#recomend")
+    const accion = document.querySelector("#accion")
+    const aventura = document.querySelector("#aventura")
+    const comedia = document.querySelector("#comedia")
+    tempData.forEach(pelicula => {
+        const tarjeta = crearObjeto(pelicula)
+        recomendados.appendChild(tarjeta)})
+        tempData.forEach(pelicula => {
+            if (pelicula.genre.includes("action")){
+                const tarjeta = crearObjeto(pelicula)
+                accion.appendChild(tarjeta)
+            }
+        })
+        tempData.forEach(pelicula => {
+            if (pelicula.genre.includes("adventure")){
+                const tarjeta = crearObjeto(pelicula)
+                aventura.appendChild(tarjeta)
+            }
+        })
+        
+        tempData.forEach(pelicula => {
+            if (pelicula.genre.includes("comedy")){
+                const tarjeta = crearObjeto(pelicula)
+                comedia.appendChild(tarjeta)
+            }
+        })
+}
+
+document.addEventListener('DOMContentLoaded', renderPeliculas)
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -29,4 +29,65 @@ const tempData = [
     poster:
       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
+  
+  {
+    title: "The Conjuring",
+    year: 2013,
+    director: "James Wan",
+    duration: "1h 52min",
+    genre: [],
+    rate: 7.5,
+    poster:
+    "https://imgs.search.brave.com/tOuxe_i7i_uzrGXelxzB_7uU9ThllC7uOIBsq_Pd8Q8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kZXBv/ci5jb20vcmVzaXpl/ci9adHpvVG5Pc2ZL/aWxQXzJuZE9oVy1F/ZVc5SzA9LzQ2NXgw/L3NtYXJ0L2ZpbHRl/cnM6Zm9ybWF0KGpw/ZWcpOnF1YWxpdHko/NzUpL2Nsb3VkZnJv/bnQtdXMtZWFzdC0x/LmltYWdlcy5hcmNw/dWJsaXNoaW5nLmNv/bS9lbGNvbWVyY2lv/L0pZSzRXQVJOTzVG/UFJCVEFSWTQyQk5E/Tk1RLmpwZw"
+},
+{
+    title: "Fast and Furius X",
+    year: 2023,
+    director: "Louis Leterrier",
+    duration: "2h 21min",
+    genre: ["action", "adventure"],
+    rate: 5.7,
+    poster:
+    "https://imgs.search.brave.com/Y0yYIjytSCDg8kpkHsvE8qBh-ZH3RCYlnrBaT13hLI4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kZXBv/ci5jb20vcmVzaXpl/ci9FdmRGOXY0VGZL/VFpMMnZzMDJVc2Y2/di1WOWs9LzYyMHgw/L3NtYXJ0L2ZpbHRl/cnM6Zm9ybWF0KGpw/ZWcpOnF1YWxpdHko/NzUpL2Nsb3VkZnJv/bnQtdXMtZWFzdC0x/LmltYWdlcy5hcmNw/dWJsaXNoaW5nLmNv/bS9lbGNvbWVyY2lv/L1hTSE1CUURJNkJF/T0xHVU5BRDdYSVRC/QkVJLnBuZw"
+},
+{
+    title: "Inside Out",
+    year: 2015,
+    director: "Pete Docter",
+    duration: "1h 34min",
+    genre: ["adventure"],
+    rate: 8.1,
+    poster:
+    "https://imgs.search.brave.com/IGqIhPn2Diqu4-k5vhidzlLGDa4Lpl1WNwPj_7W9hQE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bGFuYWNpb24uY29t/LmFyL3Jlc2l6ZXIv/djIvaW50ZW5zYS1t/ZW50ZS0yLWxvcy1k/ZXRhbGxlcy1jdXJp/b3NpZGFkZXMteS10/b2RvLTREVVZZTFpS/WVZFV0ZQNlJPUk5J/SFVCR1NFLkpQRUc_/YXV0aD0wMGMxZGM0/ZjU0NDg5MzI1MDMz/OWEyM2IxMjZkZjMz/OWJlOTk5ZDExNGY5/Y2YzODFiYWYwZTVj/N2E3ZjU4NTJiJndp/ZHRoPTQyMCZoZWln/aHQ9MjgwJnF1YWxp/dHk9NzAmc21hcnQ9/dHJ1ZQ"
+},
+{
+    title: "The Joker",
+    year: 2019,
+    director: "Tod Phillips",
+    duration: "2h 2min",
+    genre: [ "adventure", "comedy"],
+    rate: 8.4,
+    poster:
+    "https://imgs.search.brave.com/pY16lIS6QCtfIVCIVnoKsP66-xCqfyFT2dHVTtLvtIk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLmJs/b2dzLmVzL2I2YjNj/My9qb2tlci1qb2Fx/dWluLXBob2VuaXgv/NDUwXzEwMDAuanBl/Zw"
+},
+{
+    title: "Shrek",
+    year: 2001,
+    director: "Andrew Adamson",
+    duration: "1h 31min",
+    genre: ["comedy"],
+    rate: 7.9,
+    poster:
+    "https://imgs.search.brave.com/kSlLbeqsd2O2yoDt_whR8seDp465CtgyxrlaeJO_YSw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9waWNm/aWxlcy5hbHBoYWNv/ZGVycy5jb20vNDIy/LzQyMjU1MS5qcGc"
+},
+{
+    title: "The Amazing Spider-Man",
+    year: 2014,
+    director: "Marc Webb",
+    duration: "2h 22min",
+    genre: ["fantasy", "sci-fi"],
+    rate: 6.6,
+    poster:
+    "https://imgs.search.brave.com/Uf0QwAh9jJCUD_DxTyJzsZQQkgCiCYaI7remxKet9NY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL00v/TVY1Qk1UUXpNREky/TVRZeU4xNUJNbDVC/YW5CblhrRnRaVGd3/TlRReU9ETTFNVEVA/LmpwZw"
+},
 ];
